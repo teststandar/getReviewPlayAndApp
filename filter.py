85903df1e2f6e7e4942ea34c8edad50bb7d51355
teststandar <118343368+teststandar@users.noreply.github.com>
2024-10-16 14:21:47 +0700

    add new func for get data android

--- filter.py
+++ filter.py
@@ -11,6 +11,7 @@ def filter_reviews_by_date_ios(reviews, start_date, end_date):
     filtered_reviews = []
     for review in reviews:
         date_format = "%Y-%m-%dT%H:%M:%S%z"
+        # date_format = "%Y-%m-%d %H:%M:%S"
         review_date = datetime.strptime(review['attributes']['createdDate'], date_format)
         
         # Check if the review date is within the specified range
@@ -18,8 +19,11 @@ def filter_reviews_by_date_ios(reviews, start_date, end_date):
             filtered_review = {
                 "Author": review['attributes']['reviewerNickname'],
                 "Rating": review['attributes']['rating'],
+                # "Title": review['attributes']['title'],
                 "Review": review['attributes']['body'],
-                "Date": review['attributes']['createdDate'],
+                # "Territory": review['attributes']['territory'],
+                # "Date": review['attributes']['createdDate'],
+                "Date": datetime.strptime(review['attributes']['createdDate'], date_format).strftime("%Y-%m-%d %H:%M:%S")
             }
             filtered_reviews.append(filtered_review)
 
@@ -46,7 +50,73 @@ def filter_reviews_by_date_andro(reviews, start_date, end_date):
                 "Rating": review['score'],
                 "Review": review['content'],
                 "Date": review['at'],
+                # "Reply Review": review['replyContent'],
+                # "Reply Date": review['repliedAt'],
+                # "App Version": review['appVersion'],
             }
             filtered_reviews.append(filtered_review)
+
+    return filtered_reviews
+
+def filter_reviews_by_date_andro2(reviews, start_date, end_date):
+    start_date1 = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date1 = datetime.strptime(end_date, "%Y-%m-%d")
+    timezone_utc_plus_7 = pytz.timezone('Asia/Bangkok')
+    start_date_7 = timezone_utc_plus_7.localize(start_date1)
+    end_date_7 = timezone_utc_plus_7.localize(end_date1)
+    
+    filtered_reviews = []
+    for review in reviews:
+        comments = review.get('comments', [0])
+        comment = comments[0]
+        user_comment = comment.get('userComment', {})
+        text = user_comment.get('text', '')
+        star_rating = user_comment.get('starRating', 'No rating')
+        last_modified = user_comment.get('lastModified', {}).get('seconds', 'No date')
+        # for comment in comments:
+        #     user_comment = comment.get('userComment', {})
+        #     text = user_comment.get('text', '')
+        #     star_rating = user_comment.get('starRating', 'No rating')
+        #     last_modified = user_comment.get('lastModified', {}).get('seconds', 'No date')
+        # if review.get('comments', [1]) is not None:
+        if len(comments) > 1:
+            comment_2 = comments[1]
+            developer_comment = comment_2.get('developerComment', {})
+            replyContent = developer_comment.get('text', '')
+            repliedAt = developer_comment.get('lastModified', {}).get('seconds', 'No date')
+            dt_object_dev = datetime.fromtimestamp(int(repliedAt))
+        else:
+            replyContent = None
+            dt_object_dev = None
+
+        if last_modified == "No date":
+            filtered_review = {
+                "Author": review.get('authorName', 'Unknown'),
+                "Rating": star_rating,
+                "Review": text,
+                "Date": last_modified,
+                "Reply Review": replyContent,
+                "Reply Date": dt_object_dev,
+                "App Version" : user_comment.get('appVersionName', ''),
+            }
+            filtered_reviews.append(filtered_review)
+        else:
+            dt_object = datetime.fromtimestamp(int(last_modified))
+            dateString = str(dt_object)
+            date_format = "%Y-%m-%d %H:%M:%S"
+            review_date = datetime.strptime(dateString, date_format)
+            review_date_7 = timezone_utc_plus_7.localize(review_date)
+            if start_date_7 <= review_date_7 <= end_date_7:
+                filtered_review = {
+                    "Author": review.get('authorName', 'Unknown'),
+                    "Rating": star_rating,
+                    "Review": text,
+                    "Date": dt_object,
+                    "Reply Review": replyContent,
+                    "Reply Date": dt_object_dev,
+                    "App Version" : user_comment.get('appVersionName', ''),
+                }
+                filtered_reviews.append(filtered_review)
+                    
 
     return filtered_reviews
